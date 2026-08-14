6b9400956652cf186b62bf9c1990dbd4d87dec77
massahat tube <bousarhane.zitouni@gmail.com>
2026-08-14 12:04:25 +0100

    Initialize native save dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ mod models;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             db::init(app.handle()).map_err(std::io::Error::other)?;
             Ok(())
